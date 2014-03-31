7c01482118775ab3a83aa0287ad3761f548d8483
lacemaker <merlettaia@gmail.com>
2014-03-31 15:49:34 +0400

    added full solution for practice tasks

--- functional programming/hw4/practice.hs
+++ functional programming/hw4/practice.hs
@@ -1,6 +1,6 @@
-﻿{-# OPTIONS_GHC -XStandaloneDeriving #-}
-module Practice4 where
+﻿module Practice4 where
 import Data.Monoid
+import Data.List(insert)
 -- Моноиды
 -- написать 
 --newtype Endo a = Endo { appEndo :: a -> a }
@@ -52,12 +52,64 @@ reverse' :: [a] -> [a]
 reverse' = foldr (\l c -> c ++ [l]) []
 
 reverse'' :: [a] -> [a]
-reverse'' = foldl (\c l -> [l]++ c) []
+reverse'' = foldl (\c l -> [l] ++ c) []
 
 -- I (2 балла) Напишите реализацию foldl через foldr.
-foldl' f = foldr (flip f)
-
-
+-- первое отличие, которое бросается в глаза - это порядок аргументов в входной функции. 
+-- можно попробовать применять к ней функцию flip, но
+-- foldl' f = foldr (flip f)
+-- - так все равно не работает, поскольку foldl и foldr применяют эту функцию к элементам списка в разном порядке. 
+-- значит, надо изменить порядок элементов еще и в списке! то есть использовать функцию reverse через foldr, определенную выше.
+foldl' = \f x l -> foldr (flip f) x (reverse' l) -- в принципе reverse' можно расписать через foldr
+-- для проверки, reverse''' с использованием этой функции:
 reverse''' :: [a] -> [a]
 reverse''' = foldl' (\l c -> [c]++ l) []
 
+-- (3 балла) Напишите реализацию функции foldTree - свертки для двоичного дерева
+data Tree a = Nil | Branch (Tree a) a (Tree a) deriving (Eq, Show)
+-- сколько разных версий такой функции вы можете предложить?
+-- Используя foldTree, напишите функцию flattenTree:: Tree a->[a]
+-- Сколько разных версий такой функции вы можете написать?
+
+-- Решение
+-- сходу можно написать 3!=6 версий, характеризующихся разным порядком обхода дерева
+-- g :: b -> a -> b -- b- аккумулятор, a - элемент дерева
+-- вариант 1
+foldTree' :: (b -> a -> b) -> b -> Tree a -> b
+foldTree' _ x Nil = x  
+foldTree' g x (Branch lb v rb) = foldTree' g (g (foldTree' g x rb) v) lb
+-- вариант 2
+foldTree'' :: (b -> a -> b) -> b -> Tree a -> b
+foldTree'' _ x Nil = x  
+foldTree'' g x (Branch lb v rb) = foldTree'' g (g (foldTree'' g x lb) v) rb
+
+-- вариант 3
+foldTree''' :: (b -> a -> b) -> b -> Tree a -> b
+foldTree''' _ x Nil = x  
+foldTree''' g x (Branch lb v rb) = g (foldTree''' g (foldTree''' g x rb) lb) v
+
+-- вариант 4
+foldTree'4 :: (b -> a -> b) -> b -> Tree a -> b
+foldTree'4 _ x Nil = x  
+foldTree'4 g x (Branch lb v rb) = g (foldTree'4 g (foldTree'4 g x lb) rb) v
+
+-- вариант 5
+foldTree'5 :: (b -> a -> b) -> b -> Tree a -> b
+foldTree'5 _ x Nil = x  
+foldTree'5 g x (Branch lb v rb) = foldTree'5 g (foldTree'5 g (g x v) rb) lb
+
+-- вариант 6
+foldTree'6 :: (b -> a -> b) -> b -> Tree a -> b
+foldTree'6 _ x Nil = x  
+foldTree'6 g x (Branch lb v rb) = foldTree'6 g (foldTree'6 g (g x v) lb) rb
+-- еще 6 вариантов можно написать, взяв в качестве функции f = flip g (то есть функцию с другим порядком аргументов)
+-- еще можно в качестве функции-обработчика взять функцию, принимающую аккумулятор, результат обработки правого поддерева, 
+-- результат обработки левого поддерева и значение в узле. Но я пока не придумала, как ее лучше написать 
+--(и по какому принципу в этом случае увеличивать аккумулятор).
+
+flattenTree :: Tree a -> [a]
+flattenTree = foldTree' (\acc v -> acc ++ [v]) []
+
+flattenTree' :: Ord a => Tree a -> [a]
+flattenTree' t = (foldTree'5 (flip insert ) [] t)
+-- ну и то же самое можно написать с разными вариантами foldTree...
