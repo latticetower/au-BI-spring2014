1f0b583ff687a56eeedf870485365b6ca91dc927
lacemaker <merlettaia@gmail.com>
2014-04-13 10:20:17 +0400

    fixed stupid errors with shared_ptrs

--- bioinformatic algorithms/hw4/problem15_tandem_repeats_in_string/suffix_tree.h
+++ bioinformatic algorithms/hw4/problem15_tandem_repeats_in_string/suffix_tree.h
@@ -1,26 +1,39 @@
 #pragma once
 #include <memory>
-
+#include <set>
 
 //ititially we assume that all vertices describe the same string substrings
 template <class Graph>
 struct tree_vertex{
-  //std::shared_ptr<tree_vertex> next[4];// use simple hash for storing next elements. we have fixed size alphabet, so can use it
   std::shared_ptr<tree_vertex> next_vertex; // really im not sure now what to store
   std::shared_ptr<tree_vertex> neighbour; //next neighbour for current vertex
-  size_t start_pos, _length;
-  
+  size_t length;
   Graph * owner;
 
+  std::set<size_t> start_positions;
+ 
+  //size_t start_pos;  
+  
   void set_parent(Graph * g) {
     owner = g;
   }
+  ~tree_vertex() {
+    next_vertex.reset();
+    neighbour.reset();
+  }
+  tree_vertex(Graph * g, size_t start, size_t _length = 1): next_vertex(NULL), neighbour(NULL), length(_length), owner(g) { 
+    start_positions.clear();
+    start_positions.insert(start);
+    //start_pos = start;
+  }
 
-  
-  tree_vertex(Graph * g, size_t start, size_t length = 1): next_vertex(0), neighbour(0), start_pos(start), _length(length), owner(g) {  }
   tree_vertex(tree_vertex & orig) {
-    this->start_pos = orig->start_pos;
-    this->_length = orig->_length;
+    this->start_positions.clear();//TODO: check this
+    for (std::set<size_t> ::iterator iter = orig->start_positions.begin(); iter != orig->start_positions.end(); ++iter) {
+     this->start_positions.insert(*iter);
+    }
+    //this->start_pos = orig->start_pos;
+    this->length = orig->length;
     this->next_vertex = orig->next_vertex;
     this->owner = orig->owner;
     this->neighbour = orig->neighbour;
@@ -29,41 +42,29 @@ struct tree_vertex{
   tree_vertex() {
     tree_vertex(NULL, 0);
   }
-  std::shared_ptr<tree_vertex> add_next(size_t start, size_t length = 1) {
+
+  tree_vertex<Graph>* add_next(size_t start, size_t length = 1) {
     if (next_vertex == NULL) {
-      next_vertex = std::shared_ptr<tree_vertex>(new tree_vertex(owner, start));
-      return next_vertex;
+      next_vertex = std::shared_ptr<tree_vertex<Graph> >(new tree_vertex<Graph>(owner, start));
+      return next_vertex.get();
     }
     return next_vertex->add_neighbour(start, length);
-  }
-  std::shared_ptr<tree_vertex> add_neighbour(size_t start, size_t length = 1) {
-    if (owner->compare(this->start_pos, this->_length, start, length)) {
-      return std::shared_ptr<tree_vertex<Graph> >(this);
-    }
-    if (neighbour == NULL) {
-      neighbour = std::shared_ptr<tree_vertex>(new tree_vertex<Graph>(owner, start, length));
-      return neighbour;
-    } 
-    return neighbour->add_neighbour(start, length);
 
   }
-  //utility function
-  int get_vertex_index(char letter) {
-    switch(letter) {
-    case 'A':
-    case 'a':
-      return 0;
-    case 'C':
-    case 'c':
-      return 1;
-    case 'G':
-    case 'g':
-      return 2;
-    case 'T':
-    case 't':
-      return 3;
+
+  tree_vertex<Graph> * add_neighbour(size_t start, size_t _length = 1) {
+    if (owner->compare(*start_positions.begin(), length, start, _length)) {
+      start_positions.insert(start);
+      return this;
     }
-    return -1; // or should raise some exception instead
+    if (neighbour == NULL) {
+      neighbour = std::shared_ptr<tree_vertex<Graph> >(new tree_vertex<Graph>(owner, start, length));
+      return neighbour.get();
+    } 
+    return neighbour->add_neighbour(start, length);
+  }
+
+  void simplify() {
   }
 };
 
@@ -85,19 +86,26 @@ private:
 public:
 
   
-  SuffixTree(std::string & base_str): _str(base_str.c_str()) {
+  SuffixTree(std::string & base_str) {
+    _str = std::string(base_str.c_str());
     root = std::shared_ptr<tree_vertex<SuffixTree> > (new tree_vertex<SuffixTree>(this, 0) );
     for (size_t i = 0; i < _str.length(); i++) {
       AddString(i);
     }
   }
+  
+  void Simplify() {
+    root->simplify();
+  }
+
 protected:
   void AddString(size_t start_index) {
-    std::shared_ptr<tree_vertex<SuffixTree> > current_vertex(root);
+    tree_vertex<SuffixTree>* current_vertex = root.get();
     for (size_t i = start_index; i < _str.length(); i++) {
       current_vertex = current_vertex->add_next(i);
     }
 
   }
+
   
 };
