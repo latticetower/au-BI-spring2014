4697afe2113894f56d329e5cae51d9455c674dff
lacemaker <merlettaia@gmail.com>
2014-06-15 05:07:43 +0400

    optimisations 2

--- bioinformatic algorithms/hw1/problem4/hashing.h
+++ bioinformatic algorithms/hw1/problem4/hashing.h
@@ -17,8 +17,7 @@ class RevComplHasher {
 
     //returns true if hash for main str part and compl rev string part are the same
     inline bool is_match(size_t a, size_t rev_a, size_t power) {
-      size_t res =  (((a ^ rev_a) % power)^(power - 1));
-      return res==0;
+      return a == rev_a;
     }
 
     void PrepareHash(std::string const & str) {
@@ -39,11 +38,11 @@ class RevComplHasher {
       //prepare hashes:
       for (size_t i = 0; i < string_size; i++) {
         size_t current_letter = get_letter_hash(str[i]);
-        size_t rev_letter = get_letter_hash(str[string_size - i - 1]);
+        size_t rev_letter = get_letter_hash(str[string_size - i - 1], true);
         for (size_t k = 0; k < HASHES_AMOUNT; k++) {
           if (i > k) {
             hash_container[k][i - k] = ((hash_container[k][i - k - 1] << 2) % powers[k]) ^ current_letter;
-            reverse_hash_container[k][i - k] = ((reverse_hash_container[k][i-k-1] << 2) % powers[k]) ^ rev_letter;
+            reverse_hash_container[k][i - k] = ((reverse_hash_container[k][i - k - 1] << 2) % powers[k]) ^ rev_letter;
           }
           else {
             hash_container[k][0] = (hash_container[k][0] << 2) ^ current_letter;
@@ -51,7 +50,7 @@ class RevComplHasher {
           }
           // set next position - only for one of directions
           if (i >= k && k < HASHES_AMOUNT2) {
-            size_t res = hash_container[k][i - k]; //we are not interested in mismatches now
+            size_t res = reverse_hash_container[k][i - k]; //we are not interested in mismatches now
             if (last_positions[k][res] < string_size && last_positions[k][res] < i - k) 
               next_positions[k][last_positions[k][res]] = i - k;
             last_positions[k][res] = i - k;
@@ -65,7 +64,7 @@ class RevComplHasher {
     std::pair<size_t, size_t> get_max_substr(size_t min_length, size_t max_length, size_t last_pos) {
       if (max_length > min_length + 1) {
         size_t median_length = min_length + (max_length - min_length) / 2;
-        size_t pos = get_substr(median_length);
+        size_t pos = get_substr(median_length, last_pos);
         return (pos < string_size? get_max_substr(median_length, max_length, pos) : get_max_substr(min_length, median_length, -1));
       }
       return std::make_pair(last_pos, min_length);
@@ -73,36 +72,42 @@ class RevComplHasher {
     
 
     //method returns index of substring of given length
-    size_t get_substr(size_t median_length) {
-      std::cout << "get substr called with length "<< median_length << std::endl;
+    size_t get_substr(size_t median_length, size_t last_pos) {
+      std::cout << "get substr called with length "<< median_length << ", starting from " << last_pos << std::endl;
       if (median_length == 0)
         return 0;
       //prepare rolling hash, modulo great prime (> 2^15 = first found via wolframalpha - 1073742037)
+      size_t start_position = 0;
+      if (last_pos < string_size)
+        start_position = last_pos;
       
       std::vector<size_t> hash, rev_hash;
+      hash.resize(string_size - median_length + 1, 0);
+      rev_hash.resize(string_size - median_length + 1, 0);
+      
       size_t buf = 0, rev_buf = 0;
       //suppose median length is greater than HASHES_AMOUNT.
      // size_t k = std::min(HASHES_AMOUNT, median_length);
       size_t i = 0;
       for (i = 0; i < median_length; i ++) {
         buf = (buf + hash_container[0][i]) % GREAT_PRIME;
-        rev_buf = (rev_buf + ((powers[0]-1) ^ reverse_hash_container[0][i])) % GREAT_PRIME;
+        rev_buf = (rev_buf +  reverse_hash_container[0][i]) % GREAT_PRIME;
       }
-      hash.push_back(buf);
-      rev_hash.push_back(rev_buf);
-      for (; i < string_size; i++) {
+      hash[start_position] = buf;
+      rev_hash[0] = rev_buf;
+      for (; start_position + i - median_length + 1 < string_size; i++) {
         buf = (buf + hash_container[0][i] - hash_container[0][i - median_length]) % GREAT_PRIME;
-        hash.push_back(buf);
-        rev_buf = (rev_buf + ((powers[0]-1) ^ reverse_hash_container[0][i]) - ((powers[0]-1) ^ reverse_hash_container[0][i - median_length])) % GREAT_PRIME;
-        rev_hash.push_back(rev_buf);
+        hash[start_position + i - median_length + 1] = buf;
+        rev_buf = (rev_buf +  reverse_hash_container[0][i] - reverse_hash_container[0][i - median_length]) % GREAT_PRIME;
+        rev_hash[i - median_length + 1] = rev_buf;
       }
 
       //iterate for each position in reverse string
-      for (size_t rev_i = 0; rev_i < string_size; rev_i ++) {
+      for (size_t i = start_position; i < string_size; i ++) {
         //rev_i is the beginning of current hash in reverse string.
         // in normal hash it is equal to position string_size - rev_i - 1.
         // and last element is string_size-rev_i-median_length
-        for (size_t i =  get_next(rev_i, 0, 0, median_length, true); i < string_size; i =  get_next(rev_i, 0, i, median_length, false)) {
+        for (size_t rev_i =  get_next(i, 0, 0, median_length, true); rev_i < string_size - i; rev_i =  get_next(i, 0, rev_i, median_length, false)) {
           if (CheckLengthRevCompl(i, rev_i, median_length, hash, rev_hash))
             return i;
         }
@@ -112,7 +117,7 @@ class RevComplHasher {
 
 
     //helper method. returns next position of hash prefix equal to last one
-    size_t get_next(size_t rev_i, size_t min_pos, size_t last_found, size_t length, bool first_call) {
+    size_t get_next(size_t i, size_t min_rev_pos, size_t last_found, size_t length, bool first_call) {
       //std::cout << i << " " << min_pos << " " << last_found << " " << length << std::endl;
       /*if (i > min_pos) {
         std::cout << "got wrong min_pos" << std::endl;
@@ -128,29 +133,29 @@ class RevComplHasher {
       if (k == 0) 
         k = 1;
       
-      if (first_call || last_found < min_pos) {
+      if (first_call || last_found < min_rev_pos) {
         size_t k = std::min(length, HASHES_AMOUNT);
-        if (hash_container.size() < k || hash_container[k-1].size() <= rev_i) {
+        if (hash_container.size() < k || hash_container[k-1].size() <= i) {
           std::cout << "got some problem with hash_container1"<<std::endl;
         }
-        size_t rev_val = reverse_hash_container[k - 1][rev_i];
-        size_t current = min_pos;
-        if (hash_container.size() < k || hash_container[k - 1].size() <= current) {
+        size_t val = hash_container[k - 1][i];
+        size_t current_rev = min_rev_pos;
+        if (reverse_hash_container.size() < k || reverse_hash_container[k - 1].size() <= current_rev) {
           std::cout << "got some problem with hash_container2"<<std::endl;
         }
-        while (current < string_size && !is_match(rev_val, hash_container[k - 1][current], powers[k - 1])) {
-          if (current + rev_i + 1 >= string_size)
+        while (current_rev < string_size && !is_match(val, reverse_hash_container[k - 1][current_rev], powers[k - 1])) {
+          if (current_rev + i + 1 >= string_size)
             return string_size + 1;
-          current++;
-          if (hash_container.size() < k || hash_container[k-1].size() <= current) {
+          current_rev ++;
+          if (reverse_hash_container.size() < k || reverse_hash_container[k - 1].size() <= current_rev) {
              std::cout << "got some problem with hash_container3"<<std::endl;
           }
 
         }
-        return current;
+        return current_rev;
       }
       if (last_found < string_size) {
-        if (hash_container.size() < k || hash_container[k - 1].size() <= last_found) {
+        if (reverse_hash_container.size() < k || reverse_hash_container[k - 1].size() <= last_found) {
           std::cout << "got some problem with hash_container last_found" << std::endl;
         }
         return next_positions[k - 1][last_found];
@@ -187,32 +192,10 @@ class RevComplHasher {
 
 
   protected:
-    bool chars_equal(size_t i, size_t j) const {
-      if (i >= hash_container[0].size() || j >= hash_container[0].size())
-        return false;
-      return hash_container[0][i] == hash_container[0][j];
-    }
-
-  
-
-    // for given small string returns if there is one mismatch or not
-    size_t find_length(size_t i, size_t j, size_t length, bool allow_mismatch) {
-      if (i > j)
-        return find_length(j, i, length, allow_mismatch);
-      if (j + length > string_size)
-        return find_length(i, j, (string_size > j ? string_size - j : 0), allow_mismatch);
-      int error_count = 0;
-      for (size_t k = 0; k < length; k++) {
-        if (hash_container[0][i + k] != hash_container[0][j + k]) {
-          error_count ++;
-          if (error_count > 1 || !allow_mismatch)
-            return k;
-        }
-      }
-      return length;
-    }
-
-    size_t get_letter_hash(char a) {
+ 
+    size_t get_letter_hash(char a, bool reversed = false) {
+      if (reversed)
+        return (get_letter_hash(a) ^ 3);
       if (a == 'A' || a == 'a')
         return 0;
       if (a=='C' || a == 'c')
