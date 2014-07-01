e23d46811651f88051c3a0ff0ea1d9114aacc37f
latticetower <merlettaia@gmail.com>
2014-07-02 01:01:42 +0400

    now works ok on big dataset

--- bioinformatic algorithms/hw5/problem17_bwt/bwt_tools.h
+++ bioinformatic algorithms/hw5/problem17_bwt/bwt_tools.h
@@ -10,124 +10,123 @@ class BWTBuilder{
     BWTBuilder(): alphabet_size(5) { }
 
     void Build(std::string const& str) {
-      text_size = str.size();
-      if (str.size() < 1)
-        return;
-   
-      _counts.resize(alphabet_size, 0);
-      _positions.resize(alphabet_size, 0);
-      for (size_t i = 0; i < text_size; i++) {
-          _counts[getCharCode(str[i])] ++;
-      }
-      _suffix_array = getSuffixArray(str);
- 
-      _fcolumn.resize(text_size);
-      _lcolumn.resize(text_size);
-      _occurences.resize(alphabet_size, std::vector<size_t>(text_size, 0));
+        text_size = str.size();
+        if (str.size() < 1)
+            return;
 
-      for (size_t i = 0; i < text_size; i++) {
-        _fcolumn[i] = str[_suffix_array[i]];
-        _lcolumn[i] =  str[(_suffix_array[i] > 0 ? _suffix_array[i] : text_size ) - 1];
-//          std::cout << _suffix_array[i] << std::endl;
-        if (i > 0) {
-          for (size_t j = 0; j < alphabet_size; j++) {
-            _occurences[j][i] = _occurences[j][i - 1];
-          }
+        _counts.resize(alphabet_size, 0);
+        _positions.resize(alphabet_size, 0);
+        for (size_t i = 0; i < text_size; i++) {
+            _counts[getCharCode(str[i])] ++;
         }
-        _occurences[getCharCode(_lcolumn[i])][i] ++;
-      }
-      size_t sum = 0;
-      for (size_t i = 0; i < alphabet_size; i ++) {
-        _positions[i] = sum;
-        sum = sum + _counts[i];
-      }
-//      std::cout << _fcolumn << std::endl  << _lcolumn << std::endl;
+        loadSuffixArray(str);
+
+        _fcolumn.resize(text_size);
+        _lcolumn.resize(text_size);
+        _occurences.resize(alphabet_size, std::vector<size_t>(text_size, 0));
+
+        for (size_t i = 0; i < text_size; i++) {
+            _fcolumn[i] = str[_suffix_array[i]];
+            _lcolumn[i] =  str[(_suffix_array[i] > 0 ? _suffix_array[i] : text_size ) - 1];
+            if (i > 0) {
+                for (size_t j = 0; j < alphabet_size; j++) {
+                    _occurences[j][i] = _occurences[j][i - 1];
+                }
+            }
+            _occurences[getCharCode(_lcolumn[i])][i] ++;
+        }
+        size_t sum = 0;
+        for (size_t i = 0; i < alphabet_size; i ++) {
+            _positions[i] = sum;
+            sum = sum + _counts[i];
+        }
     }
 
 
     std::set<size_t> find(std::string const& str) {
-      if (str.size() < 1 || text_size < 1)
-        return std::set<size_t>();
-      size_t char_code = getCharCode(str.back());
-      size_t left = 0, right = text_size - 1;
+        if (str.size() < 1 || text_size < 1)
+            return std::set<size_t>();
+        size_t char_code = getCharCode(str.back());
+        size_t left = 0, right = text_size - 1;
+        for (size_t i = 0; i < str.size(); i ++) {
+            char_code = getCharCode(str[str.size() - 1 - i]);
+            if (_counts[char_code] == 0)
+                return std::set<size_t>();
+            left = _positions[char_code] + (left > 0 ? _occurences[char_code][ left - 1]:0);
+            right = _positions[char_code] + _occurences[char_code][right] -1;
+            if (left > right || right > text_size)
+                return std::set<size_t>();
+        }
 
-      for (size_t i = 0; i < str.size(); i ++) {
-        char_code = getCharCode(str[str.size() - 1 - i]);
-        if (_counts[char_code] == 0)
-          return std::set<size_t>();
-        left = _positions[char_code] + (left > 0 ? _occurences[char_code][ left - 1]:0);
-        right = _positions[char_code] + _occurences[char_code][right] -1;
-        if (left > right || right > text_size)
-          return std::set<size_t>();
-      }
-
-      std::set<size_t> result;
-
-      for (size_t i = left; i <= right; i++) {
-        result.insert(_suffix_array[i]);
-      }
-      return result;
-      //
+        std::set<size_t> result;
+        for (size_t i = left; i <= right; i++) {
+            result.insert(_suffix_array[i]);
+        }
+        return result;
     }
 
-    int const getCharCode(char a)  {
-      if (a == 'A' || a == 'a')
-        return 0;
-      if (a == 'C' || a == 'c')
-        return 1;
-      if (a == 'G' || a == 'g')
-        return 2;
-      if (a == 'T' || a == 't')
-        return 3;
-      return 4;
+    int const getCharCode(char a) {
+        if (a == 'A' || a == 'a')
+            return 0;
+        if (a == 'C' || a == 'c')
+            return 1;
+        if (a == 'G' || a == 'g')
+            return 2;
+        if (a == 'T' || a == 't')
+            return 3;
+        return 4;
     }
+
     int const getCharCode(std::string const& str, size_t index) {
-      if (index >= str.size())
-        return getCharCode('$');
-      return getCharCode(str[index]);
+        if (index >= str.size())
+            return getCharCode('$');
+        return getCharCode(str[index]);
     }
 
     char getByCode(size_t i) {
-      std::string s = "ACGT$";
-      if (i < 0 || i >= s.size())
-        return '$';
-      return s[i];
+        std::string s = "ACGT$";
+        if (i >= s.size())
+            return '$';
+        return s[i];
     }
 
-    std::basic_string<size_t> getSuffixArray(std::string const & str) {
-      std::basic_string<size_t> suffices_positions;
-      suffices_positions.resize(str.size());
-      for (size_t i = 0; i < str.size(); i++) {
-        suffices_positions[i] = i;
-      }
-      _sort(str, suffices_positions, 0, 0, str.size());
-      return suffices_positions;
-    }
-
-    void _sort(std::string const & str, std::basic_string<size_t> & suffices_positions, size_t start_pos, size_t offset, size_t end_pos) {
-      std::vector<size_t> counter(alphabet_size, 0);
-      std::vector<size_t> pos(alphabet_size, 0);
-      for (size_t i = start_pos; i < end_pos; i ++) {
-        counter[getCharCode(str, suffices_positions[i] + offset)] ++;
-      }
-      size_t sum = 0;
-      for (size_t i = 0; i < alphabet_size; i ++) {
-        pos[i] = sum;
-        sum += counter[i];
-      }
-      std::vector<size_t> temp(suffices_positions.size(), 0);
-      for (size_t i = start_pos; i < end_pos; i ++) {
-        if (counter[getCharCode(str, suffices_positions[i] + offset)] > 0)
-          temp[pos[getCharCode(str, suffices_positions[i] + offset)]++] = suffices_positions[i];
-      }
-      for (size_t i = start_pos, j = 0; i < end_pos; i ++, j++) {
-        suffices_positions[i] = temp[j];
-      }
-      for (size_t i = 0; i < alphabet_size; i ++) {
-        if (counter[i] > 1) {
-          _sort(str, suffices_positions, start_pos + pos[i] - counter[i], offset + 1, start_pos + pos[i]);
+    void loadSuffixArray(std::string const & str) {
+        _suffix_array.resize(str.size());
+        for (size_t i = 0; i < str.size(); i++) {
+            _suffix_array[i] = i;
         }
-      }
+        _sort(str, _suffix_array, 0, 0, str.size());
+    }
+
+    void _sort(std::string const & str,
+              std::basic_string<size_t> & suffices_positions,
+              size_t start_pos,
+              size_t offset,
+              size_t end_pos) {
+        std::vector<size_t> counter(alphabet_size, 0);
+        std::vector<size_t> pos(alphabet_size, 0);
+        for (size_t i = start_pos; i < end_pos; i ++) {
+            counter[getCharCode(str, suffices_positions[i] + offset)] ++;
+        }
+
+        size_t sum = 0;
+        for (size_t i = 0; i < alphabet_size; i ++) {
+            pos[i] = sum;
+            sum += counter[i];
+        }
+        std::vector<size_t> temp(sum, 0);
+        for (size_t i = start_pos; i < end_pos; i ++) {
+            if (counter[getCharCode(str, suffices_positions[i] + offset)] > 0)
+                temp[pos[getCharCode(str, suffices_positions[i] + offset)]++] = suffices_positions[i];
+        }
+        for (size_t i = start_pos, j = 0; i < end_pos; i ++, j++) {
+            suffices_positions[i] = temp[j];
+        }
+        for (size_t i = 0; i < alphabet_size; i ++) {
+            if (counter[i] > 1) {
+                _sort(str, suffices_positions, start_pos + pos[i] - counter[i], offset + 1, start_pos + pos[i]);
+            }
+        }
     }
   private:
     size_t alphabet_size;
@@ -135,6 +134,5 @@ class BWTBuilder{
     std::basic_string<size_t> _suffix_array;//pointer to suffix array
     std::string _fcolumn, _lcolumn;
     std::vector<size_t> _counts, _positions;
-    //std::basic_string<size_t> magic_t;
     std::vector<std::vector<size_t> > _occurences;
 };
--- bioinformatic algorithms/hw5/problem17_bwt/mainfile.cpp
+++ bioinformatic algorithms/hw5/problem17_bwt/mainfile.cpp
@@ -4,44 +4,48 @@
 #include "bwt_tools.h"
 
 int main(int argc, char** argv) {
-  if (argc < 4) {
-    std::cout << "Please, provide input file name as a first parameter, and output file name as a second parameter" << std::endl;
-    return 0;
-  }
-  std::ifstream inputStream(argv[1]);
-  if (inputStream.fail()) {
-    std::cout << "Please, provide _valid_ file name as first parameter" << std::endl;
-    return 0;
-  }
-  FastaData text_fasta;
-  text_fasta.loadFromStream(inputStream);
-  inputStream.close();
-
-  std::ifstream inputStream2(argv[2]);
-  if (inputStream2.fail()) {
-    std::cout << "Please, provide _valid_ file name as second parameter" << std::endl;
-    return 0;
-  }
-  FastaData strings_fasta;
-  strings_fasta.loadFromStream(inputStream2);
-  inputStream2.close();
-  //now we have 2 fasta objects with given text and set of given substrings
-  //foo(text_fasta.data[0].second);
-  BWTBuilder builder;
-  builder.Build(text_fasta.data[0].second + "$");
-  std::ofstream output_file(argv[3]);
-  for (std::vector<std::pair<std::string, std::string> >::iterator iter = strings_fasta.data.begin(); iter != strings_fasta.data.end(); ++iter) {
-    output_file << ">" << iter->first << std::endl;
-    std::set<size_t> positions = builder.find(iter->second);
-    if (positions.size() == 0) {
-      output_file << "no occurences were found";
+    size_t start_time = time(NULL);
+    size_t end_time = 0;
+    if (argc < 4) {
+        std::cout << "Please, provide input file name as a first parameter, and output file name as a second parameter" << std::endl;
+        return 0;
     }
-    for (std::set<size_t>::iterator iter2 = positions.begin(); iter2 != positions.end(); ++iter2) {
-      output_file << *iter2 << '\t';
+    std::ifstream inputStream(argv[1]);
+    if (inputStream.fail()) {
+        std::cout << "Please, provide _valid_ file name as first parameter" << std::endl;
+        return 0;
     }
-    output_file << std::endl;
-  }
-  output_file.close();
+    FastaData text_fasta;
+    text_fasta.loadFromStream(inputStream);
+    inputStream.close();
 
-  return 0;
+    std::ifstream inputStream2(argv[2]);
+    if (inputStream2.fail()) {
+        std::cout << "Please, provide _valid_ file name as second parameter" << std::endl;
+        return 0;
+    }
+    FastaData strings_fasta;
+    strings_fasta.loadFromStream(inputStream2);
+    inputStream2.close();
+    //now we have 2 fasta objects with given text and set of given substrings
+
+    BWTBuilder builder;
+    builder.Build(text_fasta.data[0].second + "$");
+    std::ofstream output_file(argv[3]);
+    for (std::vector<std::pair<std::string, std::string> >::iterator iter = strings_fasta.data.begin(); iter != strings_fasta.data.end(); ++iter) {
+        output_file << ">" << iter->first << std::endl;
+        std::set<size_t> positions = builder.find(iter->second);
+        if (positions.size() == 0) {
+            output_file << "no occurences were found";
+        }
+        for (std::set<size_t>::iterator iter2 = positions.begin(); iter2 != positions.end(); ++iter2) {
+            output_file << *iter2 << '\t';
+        }
+        output_file << std::endl;
+    }
+    end_time = time(NULL);
+    std::cout << "total time: " << (end_time - start_time) << " seconds" << std::endl;
+    output_file.close();
+
+    return 0;
 }
